b07b18b2e4dcba9bbb0c56536165793572d82833
sahibila <sahibila@kkg-do.eu>
2024-11-04 08:40:25 +0100

    Hello There

--- src/Attraction.java
+++ src/Attraction.java
@@ -40,5 +40,6 @@ public class Attraction {
         this.price = price;
     }
 
+
 }
 
--- src/MainControl.java
+++ src/MainControl.java
@@ -10,8 +10,21 @@ public class MainControl {
         attraktion1 = new Attraction("Achterbahn", "Schnell", 35);
         attraktion2 = new Attraction("Gruselachterbahn", "gruselig", 45);
         attraktion3 = new Attraction("Riesenrad", "groß", 50);
-        System.out.println(person1.getName() + " (" + person1.getMoney() + "€) geht zu: " + attraktion1.getAttraktion() + " und "  + attraktion3.getAttraktion() + "; Preis: " + attraktion1.getPrice() + "€ + " + attraktion3.getPrice() + "€; Beschreibung: " + attraktion1.getDescription() + " und " +  attraktion3.getDescription());
-        System.out.println(person2.getName() + " (" + person2.getMoney() + "€) geht zu: " + attraktion2.getAttraktion() + " und "  + attraktion3.getAttraktion() + "; Preis: " + attraktion2.getPrice() + "€ + " + attraktion3.getPrice() + "€; Beschreibung: " + attraktion2.getDescription() + " und " +  attraktion3.getDescription());
-        System.out.println(person3.getName() + " (" + person3.getMoney() + "€) geht zu: " + attraktion1.getAttraktion() + " und "  + attraktion2.getAttraktion() + "; Preis: " + attraktion1.getPrice() + "€ + " + attraktion2.getPrice() + "€; Beschreibung: " + attraktion1.getDescription() + " und " +  attraktion2.getDescription());
+        visitAttraction();
+    }
+    public static void visitAttraction(){
+        if (person1.getMoney() >= attraktion1.getPrice()){
+            person1.setMoney(person1.getMoney() - attraktion1.getPrice());
+            System.out.println("Olaf kann " + attraktion1.getAttraktion() + " besuchen und hat dann noch " + person1.getMoney() + "€.");
+        }
+        if (person2.getMoney() >= attraktion2.getPrice()){
+            person2.setMoney(person2.getMoney() - attraktion2.getPrice());
+            System.out.println("Olaf kann " + attraktion2.getAttraktion() + " besuchen und hat dann noch " + person2.getMoney() + "€.");
+        }
+        if (person3.getMoney() >= attraktion3.getPrice()){
+            person3.setMoney(person3.getMoney() - attraktion3.getPrice());
+            System.out.println("Olaf kann " + attraktion3.getAttraktion() + " besuchen und hat dann noch " + person3.getMoney() + "€.");
+        }
+
     }
 }
